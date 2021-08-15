fd6285c44812ea0663bcfabb1edb8ee3978235f7
AshwinPansare <67206031+AshwinPansare@users.noreply.github.com>
2021-08-15 16:17:54 +0530

    Login Validation

---
+++ processmentorlogin.php
@@ -0,0 +1,31 @@
+<?php
+	session_start();
+	$loginid = $_POST['loginid'];
+	$password = $_POST['password'];
+	
+	$loginid = stripcslashes($loginid);
+	$password = stripcslashes($password);
+	
+	$link = mysqli_connect("localhost", "root", "root", "mentoring_system");
+	if(mysqli_connect_error()){
+			die("There was an error connecting to the database");
+		}else{	
+			$loginid = mysqli_real_escape_string($link,$loginid);
+			$password = mysqli_real_escape_string($link,$password);
+			$query = "SELECT * FROM mentor WHERE login_id='$loginid' and password='$password'" ;
+			$result = mysqli_query($link,$query);
+			if($result){
+				$row = mysqli_fetch_array($result);
+				if($row['login_id'] == $loginid && $row['password'] == $password){
+					$_SESSION['success']="You are now logged in";
+					$_SESSION['mloginid']=$loginid;
+					header('location: mentorhome.php');
+				}else{
+					$_SESSION['message']="Invalid loginid or password";
+					header('location: Mentor.php');
+				}
+			}else{
+				die(mysqli_error($link));
+			}
+		}
+?>
